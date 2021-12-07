67d49b8f5586a731414dd4d49019a2b32fb9589e
jinxiao <jinxiao@tencent.com>
2021-12-07 15:26:39 +0800

    pnpm

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import zhCN from "antd/lib/locale/zh_CN";
+
 import { App } from "./App";
 import "./theme/index.less";
+import { ConfigProvider } from "antd";
 
 ReactDom.render(
   <BrowserRouter>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
